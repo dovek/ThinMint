207296389992d39c1535964681cbfa0dd7bc19d7
Kyle Dove <kddove85@gmail.com>
2015-03-03 22:33:44 -0500

    March 2015 Release
    Added more logging.
    Modified to look for nickname of account first.
    Then after the name is found, search for balance based on nickname.
    Changed to record ALL accounts with nickname. mintArray file no longer
    needed.
    
    Still need to clean up code.

--- mint.py
+++ mint.py
@@ -7,20 +7,26 @@ Modification History:
 
 DATE           Author               Description   
 ===========    ===============      =============
-Aug 10 2014    Kyle Dove            Created
-Aug 11 2014    Kyle Dove            Added Logging
-                                    Desperately needs code cleanup
-                                    Completed for testing
+Aug 10 2014    Kyle Dove            Created.
+Aug 11 2014    Kyle Dove            Added Logging.
+                                    Desperately needs code cleanup.
+                                    Completed for testing.
 Aug 19 2014    Kyle Dove            Modified Wait time for site
                                     so that the site has time to
-                                    refresh
-Sep 12 2014    Kyle Dove            Fixes applied
+                                    refresh.
+Sep 12 2014    Kyle Dove            Fixes applied.
 Sep 26 2014    Kyle Dove            Updated version of Selenium for FireFox update
-                                    Incorporated BeautifulSoup for html parsing
+                                    Incorporated BeautifulSoup for html parsing.
 Dec 12 2014    Kyle Dove            Added 10 second wait to give time for web page
-                                    to load
-Dec 18 2014    Kyle Dove            Add functionality for putting accounts in an 
-                                    array to reduce repeat code
+                                    to load.
+Dec 18 2014    Kyle Dove            Added functionality for putting accounts in an 
+                                    array to reduce repeat code.
+Mar 03 2015    Kyle Dove            Added more logging.
+                                    Modified to look for nickname of account first.
+                                    Then after the name is found, search for balance based on nickname.
+                                    Changed to record ALL accounts with nickname. mintArray file no longer needed.
+
+TODO: cleanup
  
 '''
 
@@ -35,15 +41,29 @@ from xlrd import open_workbook
 from xlwt import Formula, XFStyle
 
 #setup log
+# create logger with 'spam_application'
+logger = logging.getLogger('thin_mint')
+logger.setLevel(logging.INFO)
 logdate = time.strftime("%Y%m%d")
-logfile = 'mintLog_' + logdate + '.log'
-logging.basicConfig(filename=logfile,level=logging.INFO)
-logStartTime = time.strftime('%m/%d/%Y %I:%M:%S %p')
+# create file handler which logs even debug messages
+fh = logging.FileHandler('mintLog_' + logdate + '.log')
+fh.setLevel(logging.INFO)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
 
-logging.info('Script Initiated at: ' + logStartTime)
+logger.info('Script Initiated')
 mintPath = 'BalanceCopy.xls'
 
 #get credentials
+logger.info('Getting Credentials')
 separator=":"
 fileIN = open('mintCreds', "r")
 line = fileIN.readline()
@@ -55,10 +75,18 @@ while line:
     line = fileIN.readline()
 
 #get array
-fileIN = open('mintArray', "r")
-line = fileIN.readline()
-nums = []
+logger.info('Getting Array Info')
+#fileIN = open('mintArray', "r")
+#line = fileIN.readline()
+#nums = []
+combos = []
+balances = []
 newBalances = []
+assets = []
+liabilities = []
+totalAsset = 0.0
+totalLiability = 0.0
+column = 0
 
 while line:
     nums = line.split(separator)
@@ -66,8 +94,10 @@ while line:
 
 driver = webdriver.Firefox()
 driver.get('https://wwws.mint.com/login.event?task=L')
+logger.info('Launching Web Browser')
 
 time.sleep(10)
+logger.info('Sleeping for 10 seconds')
 
 #Find Username and Password Elements by Id
 UsernameElement = driver.find_element_by_id("form-login-username")
@@ -76,30 +106,67 @@ PasswordElement = driver.find_element_by_id("form-login-password")
 UsernameElement.send_keys(user)
 PasswordElement.send_keys(passwd)
 
+logger.info('Submitting Form')
 driver.find_element_by_id("submit").click()
 
 time.sleep(10) #Waits 10 seconds so that the site has time to refresh for new data
 #TODO: Add while loop, check for refreshing indicator, once all indicators are gone, continue.
-
+logger.info('Sleeping for 10 seconds')
 html_source = driver.page_source
 
+logger.info('Initiating Soup')
 soup = BeautifulSoup.BeautifulSoup(html_source) 
-balances = soup.findAll('span', attrs={'class' : 'balance'})
-#print balances
 
-#for balance in balances:
-#    print balance.text
+nicknames = soup.findAll('span', attrs={'class' : 'nickname'})
+print(nicknames)
 
-for i in range(len(nums)):
-    index = int(nums[i])
-    balances[index] = balances[index].text
-    balances[index] = re.sub('[-]', '', balances[index])
-    balances[index] = re.sub('[$,]', '', balances[index])
-    logging.info(balances[index])
-    balances[index] = float(balances[index])
-    newBalances.append(balances[index])
+for nickname in nicknames:
+    txt = nickname.find(text=True)
+    print "Nickname: " + txt.strip()
+    parent = nickname.parent
+    parent = parent.parent
+    for balance in parent.findAll('span', attrs={'class' : 'balance'}):
+        print('Balance: ' + balance.text)
+        combo = []
+        combo.append(txt.strip())
+        combo.append(balance.text)
+        combos.append(combo)
+
+# Log ALL balances (for now)
+counter = 0
+for combo in combos:
+    counterStr = counter
+    counterStr = str(counterStr)
+    logger.info(counterStr + ' --- Nickname: ' + combo[0] + ' --- Balane: ' + combo[1])
+    balances.append(combo[1])
+    counter = counter + 1
+    # for item in combo:
+    #     print(item)
+
+logger.info('Iterating through balances')
+for balance in balances:
+    balance = re.sub('[$,]', '', balance)
+    balance = float(balance)
+    newBalances.append(balance)
+    if balance > 0:
+        assets.append(balance)
+        print(str(balance) + ' is an asset')
+    else:
+        liabilities.append(balance)
+        print(str(balance) + ' is a liability')
+
+#find total assets
+for asset in assets:
+    totalAsset = totalAsset + asset
+logger.info('Total Asset: ' + str(totalAsset))
+
+#find total liabilities
+for liability in liabilities:
+    totalLiability = totalLiability + liability
+logger.info('Total Liability: ' + str(totalLiability))
 
 #Start Excel
+logger.info('Starting Excel')
 rb = open_workbook(mintPath,formatting_info=True)
 r_sheet = rb.sheet_by_index(0)
 wb = copy(rb)
@@ -109,8 +176,8 @@ num_rows = r_sheet.nrows
 strRows = str(num_rows + 1)
 StrRowsMinOne = str(num_rows)
 
-logging.info(num_rows)
-logging.info(time.strftime("%m/%d/%Y"))
+logger.info('Number of Rows: ' + str(num_rows))
+logger.info('Date to insert: ' + time.strftime("%m/%d/%Y"))
 
 myDate = time.strftime("%m/%d/%Y")
 
@@ -129,16 +196,20 @@ for i in range (len(newBalances)):
     w_sheet.write(num_rows,column,newBalance,currencyStyle)
 
 #Total Assets
-w_sheet.write(num_rows,9,Formula('sum(B' + strRows + ':H' + strRows + ')'),currencyStyle)
+column = column + 1
+w_sheet.write(num_rows,column,totalAsset,currencyStyle)
 #Asset Change
-w_sheet.write(num_rows,10,Formula('J' + strRows + '-J' + StrRowsMinOne),currencyStyle)
+column = column + 1
+w_sheet.write(num_rows,column,Formula('O' + strRows + '-O' + StrRowsMinOne),currencyStyle)
 #Total Liabilities
-w_sheet.write(num_rows,11,newBalances[len(newBalances) - 1],currencyStyle)
-
+column = column + 1
+w_sheet.write(num_rows,column,totalLiability,currencyStyle)
+logger.info('Saving Excel')
 wb.save(mintPath)
 
-logEndTime = time.strftime('%m/%d/%Y %I:%M:%S %p')
-logging.info('Script ended at: ' + logEndTime)
+
+#logEndTime = time.strftime('%m/%d/%Y %I:%M:%S %p')
+logger.info('Finished script. Closing browser.')
 
 #Close Down
 driver.close()
