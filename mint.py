890b5b62da1f4e608720630ee38cdfc77e93c07f
Kyle Dove <kddove85@gmail.com>
2015-03-07 22:57:52 -0500

    New Features
    Busy Week. Removed hard code from column calculating asset change.
    Added code for creating new balanceCopy file if one is not found in the
    present directory.
    Modified wait for initial start from 10 seconds to while true loop check
    for change in html title.

--- mint.py
+++ mint.py
@@ -27,6 +27,13 @@ Mar 03 2015    Kyle Dove            Added more logging.
                                     Changed to record ALL accounts with nickname. mintArray file no longer needed.
 Mar 05 2015     Kyle Dove           Did some cleanup of code.
                                     Modified wait from 10 seconds to while true loop check for change in html title.
+Mar 07 2015     Kyle Dove           Busy Week. Removed hard code from column calculating asset change.
+                                    Added code for creating new balanceCopy file if one is not found in the present directory.
+                                    Modified wait for initial start from 10 seconds to while true loop check for change in html title.
+
+TODO: Add code for sending file via email once code completes
+TODO: Records at mint.com are sometimes outdated (by hours, not days). Would like to write code to refresh the javaScript on the page
+      so that the records are more accurate AKA: less outdated.
  
 '''
 
@@ -35,32 +42,52 @@ import datetime
 import re
 import logging
 import BeautifulSoup
+import os.path
 from selenium import webdriver
 from xlutils.copy import copy 
 from xlrd import open_workbook 
 from xlwt import Formula, XFStyle
+from tempfile import TemporaryFile
+from xlwt import Workbook
 
-#setup log
-# create logger with 'thin_mint'
+##setup log
+
+#create logger with 'thin_mint'
 logger = logging.getLogger('thin_mint')
 logger.setLevel(logging.INFO)
 logdate = time.strftime("%Y%m%d")
-# create file handler which logs even debug messages
+
+#create file handler which logs even debug messages
 fh = logging.FileHandler('mintLog_' + logdate + '.log')
 fh.setLevel(logging.INFO)
-# create console handler with a higher log level
+
+#create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
-# create formatter and add it to the handlers
+
+#create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
-# add the handlers to the logger
+
+#add the handlers to the logger
 logger.addHandler(fh)
 logger.addHandler(ch)
 
 logger.info('Script Initiated')
+
+#Variables
 mintPath = 'BalanceCopy.xls'
+combos = []
+balances = []
+newBalances = []
+assets = []
+liabilities = []
+totalAsset = 0.0
+totalLiability = 0.0
+column = 0
+counter = 0
+isNewBook = False
 
 #get credentials
 logger.info('Getting Credentials')
@@ -74,31 +101,22 @@ while line:
     passwd=sout[1]
     line = fileIN.readline()
 
-#get array
-logger.info('Getting Array Info')
-#fileIN = open('mintArray', "r")
-#line = fileIN.readline()
-#nums = []
-combos = []
-balances = []
-newBalances = []
-assets = []
-liabilities = []
-totalAsset = 0.0
-totalLiability = 0.0
-column = 0
-
-while line:
-    nums = line.split(separator)
-    line = fileIN.readline()
-
 driver = webdriver.Firefox()
 driver.get('https://wwws.mint.com/login.event?task=L')
 logger.info('Launching Web Browser')
 
-#TODO: Modify this sleep timer somehow
-logger.info('Sleeping for 10 seconds')
-time.sleep(10)
+html_source = driver.page_source
+
+#Wait until page is done loading before proceeding. If page isn't done loading, sleep one second and try again.
+while True:
+    logger.info('Waiting for response')
+    soup = BeautifulSoup.BeautifulSoup(html_source)
+    logger.debug('Title: ' + soup.title.text)
+    time.sleep(1)
+    if soup.title.text == 'Mint &gt; Start Here':
+        break
+    else:
+        html_source = driver.page_source
 
 #Find Username and Password Elements by Id
 UsernameElement = driver.find_element_by_id("form-login-username")
@@ -121,6 +139,8 @@ while True:
     else:
         html_source = driver.page_source
 
+#TODO: Write code to wait for javaScript to run... don't know how I'll do this.
+
 logger.info('Initiating Soup')
 soup = BeautifulSoup.BeautifulSoup(html_source) 
 
@@ -139,8 +159,7 @@ for nickname in nicknames:
         combo.append(balance.text)
         combos.append(combo)
 
-# Log ALL balances (for now)
-counter = 0
+#Log ALL balances (for now)
 for combo in combos:
     counterStr = counter
     counterStr = str(counterStr)
@@ -172,6 +191,23 @@ logger.info('Total Liability: ' + str(totalLiability))
 
 #Start Excel
 logger.info('Starting Excel')
+logger.debug('The file is present? ' + str(os.path.isfile(mintPath)))
+#If there is no file present - create the file and set the headings for each column
+if not os.path.isfile(mintPath):
+    book = Workbook()
+    sheet1 = book.add_sheet('Current Assets - Liabilities')
+    newColumn = 0
+    for combo in combos:
+        newColumn = newColumn + 1
+        sheet1.write(0,newColumn,combo[0])
+    sheet1.write(0,newColumn + 1,'Total Assets')
+    sheet1.write(0,newColumn + 2,'Asset Change')
+    sheet1.write(0,newColumn + 3,'Total Liabilities')
+    book.save(mintPath)
+    book.save(TemporaryFile())
+    isNewBook = True
+    logger.info('New book created')
+
 rb = open_workbook(mintPath,formatting_info=True)
 r_sheet = rb.sheet_by_index(0)
 wb = copy(rb)
@@ -203,15 +239,21 @@ for i in range (len(newBalances)):
 #Total Assets
 column = column + 1
 w_sheet.write(num_rows,column,totalAsset,currencyStyle)
+
 #Asset Change
 column = column + 1
-w_sheet.write(num_rows,column,Formula('O' + strRows + '-O' + StrRowsMinOne),currencyStyle)
+previousColumn = chr(column + 96)
+logger.debug('PreviousCloumn: ' + previousColumn)
+if isNewBook:
+    w_sheet.write(num_rows,column,0,currencyStyle)
+else:    
+    w_sheet.write(num_rows,column,Formula(previousColumn + strRows + '-' + previousColumn + StrRowsMinOne),currencyStyle)
+
 #Total Liabilities
 column = column + 1
 w_sheet.write(num_rows,column,totalLiability,currencyStyle)
 logger.info('Saving Excel')
 wb.save(mintPath)
-
 
 #logEndTime = time.strftime('%m/%d/%Y %I:%M:%S %p')
 logger.info('Finished script. Closing browser.')
@@ -219,4 +261,6 @@ logger.info('Finished script. Closing browser.')
 #Close Down
 driver.close()
 
-print('Done')
+#TODO: Write code to email the file to your account...
+
+logger.info('Done')
